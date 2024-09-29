98a226ff9134c783fd4f56beb498b5e7b124c728
ramonmoreno1 <ramonmoreno1@hotmail.es>
2024-09-29 06:31:02 +0200

    final practica objetos predefinidos cliente

--- DWC/Practica2.04/ejercicio6-numeros.js
+++ DWC/Practica2.04/ejercicio6-numeros.js
@@ -1,10 +1,27 @@
-function comprobarCapicua(n) {
-
+'use strict'
+const rangoAbuscar = 100000
+function generadorDeArrays(rango) {
+    const arr = []
+    for (let i = 0; i < rangoAbuscar; i++) {
+        arr.push(i)
+    }
+    return arr
 }
-var n = 780
-let sn = n.toString().split('').reverse()
-let s = n.toString().split('')
-const boo = sn.map((e, i) => e === s[i]);
+function esPrimo(n) {
+    if (n <= 1) return false
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) {
+            return false
+        }
+    }
+    return true
+}
+function esCapicua(n) {
+    const strNum = n.toString()
+    return strNum === strNum.split('').reverse().join('')
+}
 
+const numeros = generadorDeArrays(rangoAbuscar)
 
-console.log(boo)
+const primosYCapicuas = numeros.filter(n => esPrimo(n) && esCapicua(n))
+console.log(primosYCapicuas)
