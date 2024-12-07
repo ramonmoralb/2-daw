f48153dbf21788faf49d0d438e82e1489dc2fd28
ramonmoreno1 <ramonmoreno1@hotmail.es>
2024-12-07 08:53:02 +0100

    Inicio práctica4.09 cliente

---
+++ DWC/Practica4.09/React-useEffect/src/App.jsx
@@ -0,0 +1,11 @@
+import { Localizador } from './ejercicio1/Localizador'
+import './App.css'
+
+function App() {
+
+
+  return <Localizador />
+
+}
+
+export default App
---
+++ DWC/Practica4.09/React-useEffect/src/ejercicio1/Localizador.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react"
+
+
+const Localizador = () => {
+    const [posY, setPosY] = useState(0)
+    const [posX, setPosx] = useState(0)
+    document.addEventListener('mousemove', (e) => {
+        setPosx(e.clientX)
+        setPosY(e.clientY)
+    })
+
+    return <div>
+        <div>
+            <h2>Posición del puntero</h2>
+            <p>Posición x: {posX}</p>
+            <p>Posición y: {posY}</p>
+        </div>
+    </div>
+
+}
+export { Localizador }
