025f0391f3d8845afb13af8c6d341203c401917e
ramonmoreno1 <ramonmoreno1@hotmail.es>
2025-01-10 19:27:23 +0100

    Cliente 6.01 pendiente de css, organizar y comprobar posibles errores

--- DWC/Practica6.01/app.js
+++ DWC/Practica6.01/app.js
@@ -1,16 +1,25 @@
-import { validarForm, anadirLocalStore, borrarAgenda, listar, borrarContacto } from "./funciones/funciones.js";
+import { validarForm, anadirLocalStore, borrarAgenda, listar, borrarContacto, buscarContacto, mostrarContacto, editarContacto } from "./funciones/funciones.js";
 document.addEventListener("DOMContentLoaded", () => {
     const anadirBoton = document.getElementById("boton-anadir");
     const botonBorrarAgenda = document.getElementById("boton-borrar-agenda");
     const botonListar = document.getElementById("boton-listar");
+    const botonBuscar = document.getElementById("boton-buscar");
+    const botonEditar = document.getElementById("boton-editar");
     const botonesBorrarEntrada = document.querySelectorAll("boton-borrar-entrada");
     botonesBorrarEntrada.forEach(boton =>
         boton.addEventListener("click", (e) => { borrarContacto(e) })
     )
+    const isForm = (formulario) => {
+        formulario.classList.contains("visible") ? formulario.classList.remove("visible") : formulario.classList.add("visible");
+    }
 
     botonListar.addEventListener("click", listar)
+
     botonBorrarAgenda.onclick = borrarAgenda;
     const form = document.getElementById("formulario-anadir");
+    const formBuscar = document.getElementById("formulario-buscar");
+    const formEditar = document.getElementById("formulario-editar");
+
     form.addEventListener("submit", (e) => {
         e.preventDefault();
         const nombre = e.target.nombre.value;
@@ -26,8 +35,26 @@ document.addEventListener("DOMContentLoaded", () => {
             anadirLocalStore(contacto)
         }
     })
-    const isForm = () => {
-        form.classList.contains("visible") ? form.classList.remove("visible") : form.classList.add("visible");
-    }
-    anadirBoton.addEventListener("click", isForm);
+
+    formBuscar.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const nombre = e.target.nombre.value;
+        const apellidos = e.target.apellidos.value;
+        const direccion = e.target.direccion.value;
+        const telefono = e.target.telefono.value;
+        const contacto = { nombre, apellidos, direccion, telefono };
+        buscarContacto(contacto, mostrarContacto);
+
+        e.target.nombre.value = "";
+        e.target.apellidos.value = "";
+        e.target.direccion.value = "";
+        e.target.telefono.value = "";
+
+    })
+
+
+    anadirBoton.addEventListener("click", () => isForm(form));
+    botonBuscar.addEventListener("click", () => isForm(formBuscar));
+
+
 })
--- DWC/Practica6.01/funciones/funciones.js
+++ DWC/Practica6.01/funciones/funciones.js
@@ -1,12 +1,14 @@
+const listaContactos = document.getElementById("lista-contactos");
+
 const validarForm = (nombre, apellidos, direccion, telefono) => {
     document.querySelector(".error-nombre").innerHTML = ""
     document.querySelector(".error-apellidos").innerHTML = ""
     document.querySelector(".error-telefono").innerHTML = ""
     document.querySelector(".error-direccion").innerHTML = ""
     let valido = true;
-    const patternNombre = /^[A-Za-zÁÉÍÓÚáéíóúÑñ]{6,}$/;
-    const patternApellidos = /^[A-Za-z\sÁÉÍÓÚáéíóúÑñ]{6,}$/;
-    const patternDireccion = /^.{6,}$/;
+    const patternNombre = /^[A-Za-zÁÉÍÓÚáéíóúÑñ]{3,}$/;
+    const patternApellidos = /^[A-Za-z\sÁÉÍÓÚáéíóúÑñ]{3,}$/;
+    const patternDireccion = /^.{3,}$/;
     if (!patternNombre.test(nombre)) {
         document.querySelector(".error-nombre").innerHTML = "El nombre debe contener 5 caracteres, y no debe contener números ni espacios."
         valido = false;
@@ -23,7 +25,28 @@ const validarForm = (nombre, apellidos, direccion, telefono) => {
     if (isNaN(telefono) && telefono.length < 9) {
         document.querySelector(".error-telefono").innerHTML = "El teléfono contener 9 dígitos númericos"
         valido = false;
+    }
+    return valido;
+}
+const validarPromt = (nombre, apellidos, direccion, telefono) => {
 
+    let valido = true;
+    const patternNombre = /^[A-Za-zÁÉÍÓÚáéíóúÑñ]{3,}$/;
+    const patternApellidos = /^[A-Za-z\sÁÉÍÓÚáéíóúÑñ]{3,}$/;
+    const patternDireccion = /^.{3,}$/;
+    if (!patternNombre.test(nombre)) {
+
+        valido = false;
+    }
+    if (!patternApellidos.test(apellidos)) {
+        valido = false;
+
+    } if (!patternDireccion.test(direccion)) {
+        valido = false;
+
+    }
+    if (isNaN(telefono) && telefono.length < 9) {
+        valido = false;
     }
     return valido;
 }
@@ -34,10 +57,12 @@ const anadirLocalStore = (contacto) => {
     }
     agenda.push(contacto);
     localStorage.setItem("agenda", JSON.stringify(agenda))
-    console.log(agenda)
+    actualizarLista();
+
 }
 const borrarAgenda = () => {
     localStorage.clear();
+    actualizarLista();
 }
 const contactoToLi = (contacto, indice) => {
     const { nombre, apellidos, direccion, telefono } = contacto;
@@ -47,10 +72,14 @@ const contactoToLi = (contacto, indice) => {
     const pDireccion = document.createElement("p");
     const pTelefono = document.createElement("p");
     const botonBorrarContacto = document.createElement("button");
+    const botonEditarContacto = document.createElement("button");
     botonBorrarContacto.innerHTML = "Borrar";
+    botonEditarContacto.innerHTML = "Editar";
+    botonEditarContacto.addEventListener("click", () => { editarContacto(indice) })
     botonBorrarContacto.addEventListener("click", () => {
         borrarContacto(indice);
     })
+
     pNombre.innerHTML = `Nombre: ${nombre}`;
     pApellidos.innerHTML = `Apellidos: ${apellidos}`;
     pDireccion.innerHTML = `Dirección: ${direccion}`;
@@ -60,26 +89,92 @@ const contactoToLi = (contacto, indice) => {
     li.appendChild(pDireccion);
     li.appendChild(pTelefono);
     li.appendChild(botonBorrarContacto);
+    li.appendChild(botonEditarContacto);
     return li;
 }
+
 const listar = () => {
-    const listaContactos = document.getElementById("lista-contactos");
+    if (listaContactos.style.display === "none" || listaContactos.style.display === "") {
+
+        listaContactos.style.display = "block";
+        listaContactos.innerHTML = "";
+        var agenda = JSON.parse(localStorage.getItem("agenda"));
+        if (agenda === null) {
+            agenda = [];
+        }
+        if (agenda.length === 0) {
+            listaContactos.innerHTML = "<li>Lista vacía.</li>";
+        } else {
+            agenda.forEach((contacto, indice) => {
+                listaContactos.appendChild(contactoToLi(contacto, indice));
+            });
+        }
+    } else {
+        listaContactos.style.display = "none";
+    }
+}
+const actualizarLista = () => {
     listaContactos.innerHTML = "";
     var agenda = JSON.parse(localStorage.getItem("agenda"));
     if (agenda === null) {
         agenda = [];
     }
-
-    agenda.forEach((contacto, indice) => {
-        listaContactos.appendChild(contactoToLi(contacto, indice));
-    })
-
+    if (agenda.length === 0) {
+        listaContactos.innerHTML = "<li>Lista vacía.</li>";
+    } else {
+        agenda.forEach((contacto, indice) => {
+            listaContactos.appendChild(contactoToLi(contacto, indice));
+        });
+    }
 }
+
+
 const borrarContacto = (indiceContacto) => {
     const agenda = JSON.parse(localStorage.getItem("agenda"))
     agenda.splice(indiceContacto, 1);
+
     localStorage.setItem("agenda", JSON.stringify(agenda))
-    listar();
+    actualizarLista();
+}
+const mostrarContacto = (contactoFiltrados) => {
+    listaContactos.innerHTML = "";
+    listaContactos.classList.remove("visible");
+    if (contactoFiltrados.length > 0) {
+        contactoFiltrados.forEach(contacto => listaContactos.appendChild(contactoToLi(contacto)))
+    }
 }
 
-export { validarForm, anadirLocalStore, borrarAgenda, listar, borrarContacto };
+const buscarContacto = (contactoObj, fn) => { // recibe la función necesaria segun lo que se necesite
+    const agenda = JSON.parse(localStorage.getItem("agenda"))
+    if (agenda !== null && agenda.length > 0) {
+        const resultado = agenda.filter((contacto) => contacto.nombre === contactoObj.nombre || contacto.apellidos === contactoObj.apellidos || contacto.telefono === contactoObj.telefono);
+        fn(resultado);
+    } else {
+        return [];
+    }
+}
+const editarContacto = (indice) => {
+    var agenda = JSON.parse(localStorage.getItem("agenda"));
+    var nombre = prompt("Nuevo nombre:")
+    var apellidos = prompt("Nuevos apellidos:")
+    var direccion = prompt("Nuevo dirección:")
+    var telefono = prompt("Nuevo Teléfono:")
+    if (validarPromt(nombre, apellidos, direccion, telefono)) {
+        if (nombre === null) {
+            nombre = agenda[indice].nombre;
+        } if (apellidos === null) {
+            apellidos = agenda[indice].apellidos;
+        } if (direccion === null) {
+            direccion = agenda[indice].direccion;
+        } if (telefono === null) {
+            telefono = agenda[indice].telefono;
+        }
+        agenda[indice] = { nombre, apellidos, direccion, telefono }
+        localStorage.setItem("agenda", JSON.stringify(agenda))
+    }
+    else {
+        alert("Los datos no cumplen con los requisitos");
+    }
+    actualizarLista();
+}
+export { validarForm, anadirLocalStore, borrarAgenda, listar, borrarContacto, buscarContacto, mostrarContacto, editarContacto };
